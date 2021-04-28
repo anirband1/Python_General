11897f6885e439d27a4ee1aee4e44589be0f2b3f
anirband1 <deshmukh.anirban@gmail.com>
2021-04-28 21:52:06 +0530

    Update insertion_sort.py

---
+++ General/insertion_sort.py
@@ -0,0 +1,19 @@
+'''
+start arranging in ascending order from the start of the list
+'''
+
+
+def insertion_sort(list_passed, descending=False):
+    for i in range(len(list_passed)):
+        for j in range(len(list_passed[:list_passed[i]])):
+            if list_passed[i] >= list_passed[j]:
+                temp = list_passed[j]
+                list_passed[j] = list_passed[i]
+                list_passed[i] = temp
+
+    return list_passed
+
+
+list_to_sort = [90, 1, -85, 0, 25, 34, 25, 6, 17, 73]
+
+print(insertion_sort(list_to_sort))
