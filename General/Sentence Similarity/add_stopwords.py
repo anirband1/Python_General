2e3ef4d5a0dffd8c6d56e1eb18692774418930aa
anirband1 <deshmukh.anirban@gmail.com>
2021-04-29 16:25:40 +0530

    Update add_stopwords.py

--- General/Sentence Similarity/add_stopwords.py
+++ General/Sentence Similarity/add_stopwords.py
@@ -1,7 +1,9 @@
 from nltk.corpus import stopwords
 
+# list of stopwords (from stopwords library)
 l_stopwords = sorted(stopwords.words("english"))
 
+# add stopwords to Stopwords.txt
 with open("Stopwords.txt", "w") as f:
     for word in l_stopwords:
         f.write(f"{word}\n")
