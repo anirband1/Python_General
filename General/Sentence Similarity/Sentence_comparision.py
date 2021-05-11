601af34956b53bc8400bc685e4721ac433196311
anirband1 <deshmukh.anirban@gmail.com>
2021-05-11 12:55:12 +0530

    Sentence comparision workd, Update suffixes

--- General/Sentence Similarity/Sentence_comparision.py
+++ General/Sentence Similarity/Sentence_comparision.py
@@ -1,8 +1,31 @@
+import sys
+
+
 # removes extra spaces
 def normalize(sentence):
     sentence = sentence.strip()
     sentence = sentence.replace("  ", " ")
-    return sentence
+    if sentence == '':
+        print("Invalid. Sentence cannot be blank")
+        sys.exit()
+    if ("  " in sentence):
+        normalize(sentence)
+    else:
+        return sentence
+
+
+def sentence_in_sentence(sentence1, sentence2):
+    if len(sentence1) <= len(sentence2):
+        if sentence1 in sentence2:
+            print("The sentences are 100% similar")
+            sys.exit()
+        else:
+            return stopwords_and_case(sentence1), stopwords_and_case(sentence2)
+    elif sentence2 in sentence1:
+        print("The sentences are 100% similar")
+        sys.exit()
+    else:
+        return stopwords_and_case(sentence2), stopwords_and_case(sentence1)
 
 
 # removes stopwords from sentences and converts them to lowercase
@@ -22,42 +45,55 @@ def cutoff(num, places):
     return temp / (10**places)
 
 
-# main processing
-def compare_in_words(list1, list2):
-    word_similarity = 0
+def compare_lists(list1, list2):
+    maximum = 0
     total = 0
-    # remove same words in both lists
-    for word in list1[::-1]:
-        if word in list2:
-            list1.remove(word)
-            list2.remove(word)
-            print(list1)
-            print(list2)
-            word_similarity += 100
-            total += 100
+    similarity = 0
 
-    # # finding how similar are 2 words in a list
-    # # ex. "pass" and "passed"; since "pass" is in "passed", they are 98% similar (number 98 is arbitrary)
-    # for word1 in list1[::-1]:
-    #     for word2 in list2[::-1]:
-    #         if len(word1) < len(word2):
-    #             if word1 in word2:
-    #                 word_similarity += 98
-    #             total += 100
-    #         elif len(word2) < len(word1):
-    #             if word2 in word1:
-    #                 word_similarity += 98
-    #             total += 100
-    #         elif len(word1) == len(word2):
-    #             word_similarity += 0
-    #             total += 100
+    for word1 in list1:
+        maximum = 0
+        for word2 in list2:
+            temp_sim = compare_words(word1, word2)
+            word_similarity = max(maximum, temp_sim)
+            maximum = word_similarity
+        similarity += word_similarity
+        total += 100
+
     try:
-        return (word_similarity / total * 100)
+        return ((similarity / total) * 100)
     except (ZeroDivisionError):
-        return float(word_similarity)
+        return (0)
 
 
-# def remaining_words
+def compare_words(word1, word2):
+    similarity = 0
+
+    # pass, pass
+    if len(word1) == len(word2) and (word1 in word2 or word2 in word1):
+        similarity = 90
+
+    elif len(word1) < len(word2) and word1 in word2:
+        suffix = word2[len(word1):]
+        # pass, passed
+        if suffix in l_suffixes:
+            similarity = 80
+        # pass, passthrough
+        else:
+            similarity = 50
+    elif len(word2) < len(word1) and word2 in word1:
+        suffix = word1[len(word2):]
+        # passed, pass
+        if suffix in l_suffixes:
+            similarity = 80
+        # passthrough, pass
+        else:
+            similarity = 50
+    # pass, encompass
+    else:
+        similarity = 0
+
+    return (similarity)
+
 
 # making a list of stopwords from Stopwords.txt file
 l_stopwords = []
@@ -66,21 +102,29 @@ with open("Stopwords.txt", "r") as f:
         word = word.split("\n")[0]
         l_stopwords.append(word)
 
+# making a list of suffixes from Suffixes.txt file
+l_suffixes = []
+with open("Suffixes.txt", "r") as f:
+    for word in f:
+        word = word.split("\n")[0]
+        l_suffixes.append(word)
+
 # main
 sentence1 = normalize(input("First sentence:  "))
 sentence2 = normalize(input("Second sentence:  "))
 
 # removing stopwords and changing case for the 2 sentences
-l_words1 = stopwords_and_case(sentence1)
-l_words2 = stopwords_and_case(sentence2)
+l_words1, l_words2 = sentence_in_sentence(sentence1, sentence2)
 
 # to see the list of words without stopwords
 # print(f"first list: {l_words1}\nsecond list: {l_words2}")
 
 # comparing every word in each sentence
-similarity_in_words = compare_in_words(l_words1, l_words2)
+similarity_in_words = compare_lists(l_words1, l_words2)
 
-print(f"The sentences are {cutoff(similarity_in_words, 3)}% similar")
+print(
+    f"The first sentence is {cutoff(similarity_in_words, 3)}% similar to the second one"
+)
 
 # TO DO:
 # Suffixes logic
