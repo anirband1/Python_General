1457b012d673aab1c239e19079842b82398baae0
anirband1 <deshmukh.anirban@gmail.com>
2021-05-21 18:06:03 +0530

    Update Color_average.py

--- Colors/Color_average.py
+++ Colors/Color_average.py
@@ -2,12 +2,16 @@ def color_avg(l_val1, l_val2):
     l_final = []
 
     for n in range(3):
-        l_final.append((l_val1[n] + l_val2[n]) / 2)
+        l_final.append(round((int(l_val1[n]) + int(l_val2[n])) / 2))
 
     return l_final
 
 
-color1 = list(input("Color 1:   "))
-color2 = list(input("Color 2:   "))
+color1 = (
+    input("Color 1:   ")).removeprefix("rgb(").removesuffix(")").split(", ")
+color2 = (
+    input("Color 2:   ")).removeprefix("rgb(").removesuffix(")").split(", ")
 
-print(f"rgb({color_avg(color1, color2)}")
+print(f"rgb({color_avg(color1, color2)})".replace("[", "").replace("]", ""))
+
+sfg = "fskfhd"
