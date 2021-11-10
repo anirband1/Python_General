f6e1cfb4e74ac5c30398ca1f0e35690cb815eb4b
anirband1 <deshmukh.anirban@gmail.com>
2021-11-10 16:17:35 +0530

    Add interpolated numbers program

---
+++ Tedious Maths/interpolated_numbers.py
@@ -0,0 +1,28 @@
+'''
+prints equidistant numbers between two numbers 
+'''
+
+import sys
+
+divs = int(input("Divisions:    "))
+
+
+def Divs():
+
+    try:
+        high = float(input("high:    "))
+        low = float(input("low:     "))
+    except:
+        sys.exit()
+
+    interpolant = (high - low) / divs
+
+    l_num = [high - (i * interpolant) for i in range(divs)]
+
+    print(l_num[1:])
+
+    Divs()
+
+
+if __name__ == "__main__":
+    Divs()
