18791f77625fa59f7c4c69214dbd9a53ae93eb0e
anirband1 <deshmukh.anirban@gmail.com>
2021-11-15 12:43:40 +0530

    Image manipulation - Add overlay, Add blur

--- Colors/Color_average.py
+++ Colors/Color_average.py
@@ -1,10 +1,8 @@
+import subprocess
+
+
 def color_avg(l_val1, l_val2):
-    l_final = []
-
-    for n in range(3):
-        l_final.append(round((int(l_val1[n]) + int(l_val2[n])) / 2))
-
-    return l_final
+    return [round((int(l_val1[n]) + int(l_val2[n])) / 2) for n in range(3)]
 
 
 color1 = (
@@ -12,6 +10,8 @@ color1 = (
 color2 = (
     input("Color 2:   ")).removeprefix("rgb(").removesuffix(")").split(", ")
 
-print(f"rgb({color_avg(color1, color2)})".replace("[", "").replace("]", ""))
+data = f"rgb({color_avg(color1, color2)})".replace("[", "").replace("]", "")
 
-sfg = "fskfhd"
+subprocess.run("pbcopy", universal_newlines=True, input=data)
+
+print(data, "copied to clipboard")
